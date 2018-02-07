e96a2c58e22f7a0117fb9c41c1e961a94cd63de6
nekocode <nekocode.cn@gmail.com>
2018-02-07 15:52:02 +0800

    Initail commit

---
+++ pure-java-lib/src/main/java/cn/nekocode/gradleimportaar/example/TestImports.java
@@ -0,0 +1,15 @@
+package cn.nekocode.gradleimportaar.example;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * @author nekocode (nekocode.cn@gmail.com)
+ */
+public class TestImports {
+
+    static void test() {
+        Context context = null;
+        RecyclerView recyclerView = new RecyclerView(context);
+    }
+}
